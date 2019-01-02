fa8d2902dbda48b513cf5b23f4f664b5a2e84f2f
KarenSarmiento <karen.sarmiento25@gmail.com>
2019-01-02 20:04:55 +0000

    Implement board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class Board {
+    private Input[][] board;
+
+    public Board() {
+        this.board = new Input[3][3];
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                board[row][col] = Board.Input.EMPTY;
+            }
+        }
+    }
+
+    public void printBoard() {
+        System.out.println("-------");
+        for (int row = 0; row < board.length; row++) {
+            System.out.print("|");
+            for (int col = 0; col < board[row].length; col++) {
+                System.out.print(board[row][col].getRepresentation() + "|");
+            }
+            System.out.println("\n-------");
+        }
+    }
+
+    public Input[][] getBoard() {
+        return board;
+    }
+
+    public enum Input {
+        EMPTY(' '), NOUGHT('o'), CROSS('x');
+
+        private char representation;
+
+        Input(char representation) {
+            this.representation = representation;
+        }
+
+        public char getRepresentation() {
+            return representation;
+        }
+    }
 }
